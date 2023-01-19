bba8fdb1b3f1d2b1ee3ade7ef937d9105dde61b7
Robert Fassel <fcwrob@gmail.com>
2023-01-18 18:55:09 -0600

    Completed Syntax Sugar Exercise

--- SyntaxAndSyntaxSugarExercise/Program.cs
+++ SyntaxAndSyntaxSugarExercise/Program.cs
@@ -4,7 +4,24 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var answer = 4;
+            var response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} is greater than or equal to nine";
+
+            //int answer = 4;
+            //string response;
+            //if (answer < 9)
+            //{
+            //    response = answer + " is less than nine";
+            //}
+            //else
+            //{
+            //    response = answer + " is greater than or equal to nine";
+            //}
+
+            // DONE Given the following code, change the code to implement:
+            //inferred typing
+            //string interpolation
+            //and the ternary operator
         }
     }
 }
